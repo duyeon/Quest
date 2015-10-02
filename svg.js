6d12117cf5dffa74f1b07eff3dbb8442c8a068e7
duyeon <duyeon@postech.ac.kr>
2015-10-02 15:08:49 +0900

    svg
    svg-스케치북 만들기 입니다

---
+++ svg.js
@@ -0,0 +1,68 @@
+function makerectangle() {
+			alert('make rectangle!');
+			var rectangles = document.createElementNS("http://www.w3.org/2000/svg","rect");
+			rectangles.setAttribute("width",40);
+    		rectangles.setAttribute("height",40);
+    		rectangles.id= 'myrectangle';
+			rectangles.setAttribute('fill','#35aee3');
+			makesvg().appendChild(rectangles);
+		}
+function makecircle(){
+			alert('make circle!');
+			var circles = document.createElementNS("http://www.w3.org/2000/svg","circle");
+			circles.setAttribute("cx",20);
+    		circles.setAttribute("cy",20);
+    		circles.setAttribute("r",20);
+    		circles.id = 'mycircle';
+			circles.setAttribute('fill','#e84496');
+			makesvg().appendChild(circles);
+		}
+function maketriangle(){
+			alert('make triangle!');
+			var triangles = document.createElementNS("http://www.w3.org/2000/svg","polygon");
+			triangles.setAttribute("points",'20,0 40,40 0,40');
+    		triangles.id = 'mytriangle';
+			triangles.setAttribute('fill','#5cc65c');
+			makesvg().appendChild(triangles);
+		}
+function makesvg(){
+	var makesvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+			makesvg.setAttribute('height',40);
+			makesvg.setAttribute('width',40);
+			makesvg.className = 'svg';
+			makesvg.style.position = 'absolute';
+			makesvg.style.left = 300+'px';
+			makesvg.style.top = 300+'px';
+			document.body.appendChild(makesvg);
+			return makesvg;
+	
+}
+
+function obj(evt){
+	
+	var obj = document.querySelector('svg');
+	var kcode = evt.keyCode;
+	
+	if(kcode == 46){
+		obj.remove();
+	}
+	else if(kcode == 37){
+		obj.style.left = parseInt(obj.style.left)-30+'px';
+	}
+	else if(kcode == 38){
+		obj.style.top = parseInt(obj.style.top)-30+'px';	
+	}
+	else if(kcode == 39){
+		obj.style.left = parseInt(obj.style.left)+30+'px';
+	}
+	else if(kcode == 40){
+		obj.style.top = parseInt(obj.style.top)+30+'px';
+	}
+
+	
+}
+function selectTarget(){
+	this.id = 'now';
+	alert(this.id);
+	
+}
