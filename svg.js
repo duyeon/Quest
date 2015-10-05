c8e8be7f46a7449bc26eb8e5265251d71051b2cf
duyeon <duyeon@postech.ac.kr>
2015-10-05 17:22:40 +0900

    svg-sketchbook
    스케치북 수정 : 도형을 클릭하여 원하는 도형을 움직이고 삭제할 수 있게 다시 구현하였습니다.

--- svg.js
+++ svg.js
@@ -6,7 +6,7 @@ function makerectangle() {
     		rectangles.id= 'myrectangle';
 			rectangles.setAttribute('fill','#35aee3');
 			makesvg().appendChild(rectangles);
-		}
+		};
 function makecircle(){
 			alert('make circle!');
 			var circles = document.createElementNS("http://www.w3.org/2000/svg","circle");
@@ -14,55 +14,56 @@ function makecircle(){
     		circles.setAttribute("cy",20);
     		circles.setAttribute("r",20);
     		circles.id = 'mycircle';
-			circles.setAttribute('fill','#e84496');
+			circles.setAttribute('fill','#35aee3');
 			makesvg().appendChild(circles);
-		}
+		};
 function maketriangle(){
 			alert('make triangle!');
 			var triangles = document.createElementNS("http://www.w3.org/2000/svg","polygon");
 			triangles.setAttribute("points",'20,0 40,40 0,40');
     		triangles.id = 'mytriangle';
-			triangles.setAttribute('fill','#5cc65c');
+			triangles.setAttribute('fill','#35aee3');
 			makesvg().appendChild(triangles);
-		}
+		};
 function makesvg(){
+	
 	var makesvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 			makesvg.setAttribute('height',40);
 			makesvg.setAttribute('width',40);
-			makesvg.className = 'svg';
+			makesvg.id = 'svg';
+			makesvg.setAttribute('class','svgclass');
 			makesvg.style.position = 'absolute';
 			makesvg.style.left = 300+'px';
 			makesvg.style.top = 300+'px';
 			document.body.appendChild(makesvg);
 			return makesvg;
-	
-}
 
+};
 function obj(evt){
 	
-	var obj = document.querySelector('svg');
+	var obj = document.querySelector('#now');
 	var kcode = evt.keyCode;
 	
 	if(kcode == 46){
 		obj.remove();
 	}
 	else if(kcode == 37){
-		obj.style.left = parseInt(obj.style.left)-30+'px';
+		obj.style.left = parseInt(obj.style.left)-20+'px';
 	}
 	else if(kcode == 38){
-		obj.style.top = parseInt(obj.style.top)-30+'px';	
+		obj.style.top = parseInt(obj.style.top)-20+'px';	
 	}
 	else if(kcode == 39){
-		obj.style.left = parseInt(obj.style.left)+30+'px';
+		obj.style.left = parseInt(obj.style.left)+20+'px';
 	}
 	else if(kcode == 40){
-		obj.style.top = parseInt(obj.style.top)+30+'px';
+		obj.style.top = parseInt(obj.style.top)+20+'px';
 	}
 
 	
-}
+};
 function selectTarget(){
+	$("*").removeAttr('id');
 	this.id = 'now';
-	alert(this.id);
-	
+
 }
